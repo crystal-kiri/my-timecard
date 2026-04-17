4b46e8be6fde6f102626c0dddd127c77c66e464b
crystal-kiri <yurika_kiriyama_g@haltalk.com>
2026-04-17 20:56:22 +0900

    Update app.py

--- app.py
+++ app.py
@@ -51,7 +51,7 @@ st.markdown(f"""
     margin: 0 auto;
 }}
 [data-testid="stMainBlockContainer"] {{
-    transform: translateY(-20px);
+    transform: translateY(-30px);
 }}
 header, footer {{ visibility: hidden !important; }}
 
